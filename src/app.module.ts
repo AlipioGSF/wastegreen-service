04e7da21e644a2d807aafa099e1d079f76360b48
AlipioGSF <alipiotec@gmail.com>
2024-04-05 17:05:25 -0300

    :recycle: update in prisma schema, service e app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { SignController } from './sign/sign.controller';
 import { SignService } from './sign/sign.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { PrismaService } from 'prisma/prisma.service';
+import { ProfileController } from './profile/profile.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, SignController, AuthController],
-  providers: [AppService, SignService, AuthService],
+  controllers: [AppController, SignController, AuthController, UserController, ProfileController],
+  providers: [AppService, SignService, AuthService, UserService, PrismaService],
 })
 export class AppModule {}
